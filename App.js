d3f1cb31b50a12e3d4c4537c89496411d5426464
Harold Angenent <harold@trendwerk.nl>
2018-05-08 14:20:00 +0200

    Create basic boxed layout

--- App.js
+++ App.js
@@ -1,23 +1,61 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, StatusBar } from 'react-native';
+
+const Project = ({ background, color = '#fff', title }) => (
+  <View style={[styles.box, { backgroundColor: background }]}>
+    <Text style={[styles.boxText, { color: color }]}>{title}</Text>
+  </View>
+)
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <StatusBar hidden={true} />
+          <Text style={styles.heading}>Side projects</Text>
+          <Project background="#4fe292" title="Building furniture" />
+          <Project background="papayawhip" color="#000" title="Chopping wood" />
+          <Project background="dodgerblue" title="Learn the flute" />
+          <View style={[styles.box, styles.boxAdd]}>
+            <Text style={styles.boxText}>+ Add new project</Text>
+          </View>
+        </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  box: {
+    borderRadius: 3,
+    marginBottom: 10,
+    minHeight: 100,
+    padding: 25,
+  },
+  boxAdd: {
+    alignItems: 'center',
+    backgroundColor: 'lightslategrey',
+    display: 'flex',
+    justifyContent: 'center',
+    height: 100,
+  },
+  boxText: {
+    color: '#fff',
+    fontSize: 22,
+    fontWeight: '600',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 30,
+    paddingTop: 40,
+  },
+  heading: {
+    fontSize: 40,
+    fontWeight: '800',
+    marginBottom: 30,
+  },
+  progressBar: {
+
   },
 });
