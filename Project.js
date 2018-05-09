09b44273ab6399e22514782f7a98f4b095eff9c6
Harold Angenent <harold@trendwerk.nl>
2018-05-09 15:17:23 +0200

    Create project detail page

--- Project.js
+++ Project.js
@@ -1,8 +1,15 @@
 import React from 'react'
-import { Text, TouchableHighlight } from 'react-native'
+import Heading from './Heading'
+import Container from './Container'
 
 export default class Project extends React.Component {
   render() {
-    return <TouchableHighlight onPress={() => this.props.navigation.goBack()}><Text>{this.props.navigation.state.params.project.title}</Text></TouchableHighlight>
+    const { project } = this.props.navigation.state.params
+
+    return (
+      <Container style={{ backgroundColor: project.background }}>
+        <Heading>{project.title}</Heading>
+      </Container>
+    )
   }
 }
