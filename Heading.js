b33513f86a215c8fc9e1c48fb883155c2f22297c
Harold Angenent <harold@trendwerk.nl>
2018-05-17 15:43:01 +0200

    :art: Use object destructuring notation

--- Heading.js
+++ Heading.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { StyleSheet, Text } from 'react-native'
 
-export default props => (
-  <Text style={[styles.heading, props.style]}>{props.children}</Text>
+export default ({ style, children }) => (
+  <Text style={[styles.heading, style]}>{children}</Text>
 )
 
 const styles = StyleSheet.create({
